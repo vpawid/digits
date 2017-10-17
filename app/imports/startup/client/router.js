e770cbbbbbca1913d75f59d71bc2f3b509f7d96a
Victor Pawid <niceguyhms@gmail.com>
2017-10-16 19:55:34 -1000

    add contact edit contact pages

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -34,3 +34,18 @@ FlowRouter.notFound = {
     BlazeLayout.render('App_Body', { main: 'App_Not_Found' });
   },
 };
+
+FlowRouter.route('/add-contact', {
+  name: 'Add_Contact_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Add_Contact_Page' });
+  },
+});
+
+FlowRouter.route('/edit-contact/:_id', {
+  name: 'Edit_Contact_Page',
+  action() {
+    BlazeLayout.render('App_Body', { main: 'Edit_Contact_Page' });
+  },
+});
+
